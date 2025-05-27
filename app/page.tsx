33109a3512ee7ff3220ec81280830c2f3a98fce4
ronik297 <ronik.allen@gmail.com>
2025-05-27 21:05:53 +0530

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl font-karla'>
+        Welcome to the Next.js 14 App Router with Tailwind CSS and TypeScript!
+      </h1>
+    </div>
   )
 }
 
