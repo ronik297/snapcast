b04ac62dd2c00cf018d0f4f0bf3a488ddd4fee44
ronik297 <ronik.allen@gmail.com>
2025-05-27 22:49:03 +0530

    added video card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
     <main className='wrapper page'>
       <Header title="All Videos" subHeader="Public Library" />
-      <h1 className='text-2xl font-karla'>
-        Welcome to the Next.js 14 App Router with Tailwind CSS and TypeScript!
-      </h1>
-    </main>
-  )
+
+      <section className='video-grid'>
+        {dummyCards.map((card) => (
+            <VideoCard
+              key={card.id}
+              {...card}
+            />
+        ))}
+      </section>
+      
+      </main>
+    )
 }
 
 export default Page
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,19 +1,23 @@
 import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from '@/constants';
 import React from 'react'
 
-type ParamsWithSearch = {
-  params: {
-    id: string;
-  };
-};
-
-const Page = async ({ params } : ParamsWithSearch) => {
-    const { id } = params;
+const Page = async () => {
 
   return (
     <div className='wrapper page'>
         <Header subHeader='adrian@jsmastery.pro' title='Adrian | JS Mastery' userImg='/assets/images/dummy.jpg'  />
-        <h1 className='text-2xl font-karla'>USER ID: {id}</h1>
+
+        <section className='video-grid'> 
+            {dummyCards.map((card) => (
+                <VideoCard
+                key={card.id}
+                {...card}
+                />
+            ))}
+        </section>
+
     </div>
   )
 }
